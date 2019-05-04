cb84e4f945f514d01d52c3c3e167ba2af944ff1f
Gigi Tannous <gtannous16@gmail.com>
2019-05-04 14:16:50 -0400

    got the log to work

--- liri.js
+++ liri.js
@@ -59,18 +59,25 @@ function readUserInput() {
 function searchBand(string) {
     axios.get(`https://rest.bandsintown.com/artists/${string}/events?app_id=codingbootcamp`).then(
       function(response) {
-        
-        
-        for (var i=0; i<5;i++){
-          console.log("**********************************************************************")
-          console.log("Name of the venue: " + response.data[i].venue.name);
-          console.log("Venue location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-          console.log("Date of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
-        }
-        
+        var divider = "\n**********************************************************************************************************************\n"
+        var concertData = response.data;
+        for(let i = 0; i < 5; i++){
+
+        var showConcertData = [
+          "Name of the venue: " + concertData[i].venue.name,
+          "Venue location: " + concertData[i].venue.city + ", " + concertData[i].venue.region,
+          "Date of the Event: " + moment(concertData[i].datetime).format("MM/DD/YYYY")
+        ].join("\n");
+         console.log(divider + showConcertData + divider);
+         fs.appendFile("log.txt",divider + showConcertData + divider, function(err) {
+        if (err) throw err;
       }
-    )
-  };
+       
+      )}
+    
+      });
+
+    };
   //Function to search data from the random.txt file (Still not working)
   function readTextFile () {
     fs.readFile("random.txt", "utf8", function(error, data) {
@@ -89,24 +96,29 @@ function searchBand(string) {
   function searchMovie(string) {
     axios.get(`http://www.omdbapi.com/?t=${string}&y=&plot=short&apikey=trilogy`).then(
       function(response) {
-        console.log("***********************************************************************************************************************************")
-        console.log("");
-        console.log("Title of the Movie: " + response.data.Title);
-        console.log("Year the Movie Came Out: " + response.data.Year);
-        console.log("IMDB Rating of the Movie: " + response.data.Ratings[0].Value);
-        console.log("Rotten Tomatoes Rating of the Movie: " + response.data.Ratings[1].Value);
-        console.log("Country where the Movie was Produced: " + response.data.Country);
-        console.log("Language of the Movie: " + response.data.Language);
-        console.log("Plot of the Movie: " + response.data.Plot);
-        console.log("Actors in the Movie: " + response.data.Actors);
-        console.log("");
-        console.log("***********************************************************************************************************************************")
+        var divider = "\n\n**********************************************************************************************************************\n\n"
+        var movieData = response.data;
+        var showMovieData = [
+          "Title of the Movie: " + movieData.Title,
+          "Year the Movie Came Out: " + movieData.Year,
+          "IMDB Rating of the Movie: " + movieData.Ratings[0].Value,
+          "Rotten Tomatoes Rating of the Movie: " + movieData.Ratings[1].Value,
+          "Country where the Movie was Produced: " + movieData.Country,
+          "Language of the Movie: " + movieData.Language,
+          "Plot of the Movie: " + movieData.Plot,
+          "Actors in the Movie: " + movieData.Actors,
+        ].join("\n\n")
+        fs.appendFile("log.txt",divider + showMovieData + divider, function(err) {
+          if (err) throw err;
+          console.log(divider + showMovieData + divider);
+        });
       }
     )
   };
   
   //Function to search song on Spotify API
   function searchSong(param) {
+    var divider = "***********************************************************************************************************************************"
     spotify.search({ type: 'track', query: param }, function(err, data) {
       if (err) {
         console.log('Error occurred: ' + err);
@@ -114,15 +126,24 @@ function searchBand(string) {
     }
     //loop through the array of songs
     console.log("");
+
     var songs = data.tracks.items;
-    for (var i=0; i<songs.length;i++){
-      console.log("***********************************************************************************************************************************")
-      console.log("Song's Name: " + songs[i].name);
-      console.log("Artist: " + songs[i].artists.map(getArtistsNames));
-      console.log("Preview Link: " + songs[i].preview_url);
-      console.log("Album: " + songs[i].album.name);
-    } 
-  
+   
+    for (var i=0; i < 5; i++){
+      var showMusicData = [
+      "\nSong's Name: " + songs[i].name,
+      "\nArtist: " + songs[i].artists.map(getArtistsNames),
+      "\nPreview Link: " + songs[i].preview_url,
+      "\nAlbum: " + songs[i].album.name
+      ]
+
+      console.log( divider + "\n" + showMusicData + "\n")
+     fs.appendFile("log.txt", divider + showMusicData + "\n", function(err) {
+      if (err) throw err;
+    });
+    }
+
+    
   });
 }
 function getArtistsNames(artist) {
